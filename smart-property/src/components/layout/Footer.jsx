157c4559305f427105ce225f163395b75795ef52
Dwi Reza <dwireza1002@gmail.com>
2026-06-07 10:51:02 +0800

    feat: implement footer into page

---
+++ smart-property/src/components/layout/Footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="flex justify-center items-center bg-bg-secondary py-24">
+      <p className="text-gray-500">
+        &copy; 2026 Smart Property by Muhammad Dwi Reza. All rights reserved.
+      </p>
+    </footer>
+  );
+}
