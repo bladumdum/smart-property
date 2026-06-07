829679bfab06c3f04add8b898bf5a8a0a74969dd
Dwi Reza <dwireza1002@gmail.com>
2026-06-07 10:37:33 +0800

    feat: create website pages

--- smart-property/src/pages/Home.jsx
+++ smart-property/src/pages/Home.jsx
@@ -1,16 +1,42 @@
+import Hero from "../components/landing-page/Hero";
+import AboutProject from "../components/landing-page/AboutProject";
+import DatasetSection from "../components/landing-page/DatasetSection";
+import ModelPerform from "../components/landing-page/ModelPerform";
+import SystemOperation from "../components/landing-page/SystemOperation";
+import GetReady from "../components/landing-page/GetReady";
+
 export default function HomePage() {
   return (
     <>
       {/* hero */}
-      <section
-        className="relative h-screen bg-cover bg-center bg-no-repeat"
-        style={{ backgroundImage: `url('../assets/images/hero.jpg')` }}>
-        <div className="absolute inset-0 bg-black/50"></div>
+      <div className="container-center ">
+        <Hero />
+      </div>
 
-        <div className=" container mx-auto h-full flex items-center justify-center relative z-10">
-          <h1 className=" text-6xl font-semibold">Prediksi Harga Rumah</h1>
-        </div>
-      </section>
+      {/* Tentang proyek */}
+      <div>
+        <AboutProject />
+      </div>
+
+      {/* dataset */}
+      <div className="container-center">
+        <DatasetSection />
+      </div>
+
+      {/* Model Perform */}
+      <div className="container-center">
+        <ModelPerform />
+      </div>
+
+      {/* sistem operation */}
+      <div className="container-center">
+        <SystemOperation />
+      </div>
+
+      {/* get ready */}
+      <div className="container-center">
+        <GetReady />
+      </div>
     </>
   );
 }
