46fcf2595717ae5734f92e3fc89c3d3eac372a6a
Dwi Reza <dwireza1002@gmail.com>
2026-06-07 10:52:22 +0800

    style: add padding into header's content container

--- smart-property/src/layouts/RootLayout.jsx
+++ smart-property/src/layouts/RootLayout.jsx
@@ -1,19 +1,22 @@
-import { Link, Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import Navbar from "../components/layout/Navbar";
+import Footer from "../components/layout/Footer";
 
 export default function RootLayout() {
-  const pages = ["Home", "Prediction", "History"];
   return (
     <div className=" min-h-screen flex flex-col">
       <header className=" sticky top-0 z-50 backdrop-blur-md bg-bg-secondary/70 h-18 flex items-center">
-        <div className=" flex justify-between container-center w-full">
+        <div className=" flex justify-between container-center w-full px-10">
           <h1>logo</h1>
           <Navbar />
         </div>
       </header>
+
       <main className="flex-1 w-full">
         <Outlet />
       </main>
+
+      <Footer />
     </div>
   );
 }
