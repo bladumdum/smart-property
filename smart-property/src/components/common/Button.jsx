4217204f33ddc4d4028aff932c38e4bd0765ed4a
Dwi Reza <dwireza1002@gmail.com>
2026-06-08 10:17:54 +0800

    style: mengubah posisi teks button ke tengah

--- smart-property/src/components/common/Button.jsx
+++ smart-property/src/components/common/Button.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function Button({ name = "Mulai Prediksi" }) {
   return (
     <Link
-      className=" bg-brand flex justify-between items-center rounded-sm h-16 px-12 py-2 text-sm font-semibold"
+      className=" bg-brand flex justify-center items-center rounded-sm h-16 px-12 py-2 text-sm font-semibold"
       to={"/prediction"}>
       {name}
     </Link>
--- smart-property/src/components/prediction/HouseForm.jsx
+++ smart-property/src/components/prediction/HouseForm.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Input from "../common/Input";
+import { predictionHouse } from "../../services/api";
 
 export default function HouseForm({ setPrediction }) {
   const [formData, setFormData] = useState({
@@ -10,10 +11,17 @@ export default function HouseForm({ setPrediction }) {
     GRS: "",
   });
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
     console.log(formData);
+
+    try {
+      const prediction = await predictionHouse(formData);
+      return setPrediction(prediction);
+    } catch (error) {
+      console.error(`error: ${error}`);
+    }
   };
 
   const handleChange = (e) => {
@@ -65,7 +73,7 @@ export default function HouseForm({ setPrediction }) {
   return (
     <div className=" pt-24 w-full">
       <form onSubmit={handleSubmit} className="w-full">
-        <div className="flex flex-col gap-4">
+        <div className="flex flex-col gap-4 mb-8">
           {inputContents.map(({ label, type, name, value, placeholder }) => (
             <div className="flex flex-col gap-2" key={name}>
               <Input
@@ -79,7 +87,11 @@ export default function HouseForm({ setPrediction }) {
             </div>
           ))}
         </div>
-        <button type="submit">Predict</button>
+        <button
+          type="submit"
+          className="bg-brand flex justify-center items-center rounded-sm w-full py-4 text-sm font-semibold">
+          Predict
+        </button>
       </form>
     </div>
   );
