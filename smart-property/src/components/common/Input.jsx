479dea3f701ec4a962a59cb49ac79f64ac2593d8
Dwi Reza <dwireza1002@gmail.com>
2026-06-08 12:15:41 +0800

    feat: implementasi fungsi pencegah perilaku Input
    - Input tipe number memiliki perilaku yang dapat mengurangi nilai input ketika user mengerakkan kursor

--- smart-property/src/components/common/Input.jsx
+++ smart-property/src/components/common/Input.jsx
@@ -8,7 +8,7 @@ export default function Input({
 }) {
   return (
     <>
-      <label htmlFor="" className="px-6">
+      <label htmlFor="" className="px-4">
         {label}
       </label>
       <input
@@ -16,8 +16,9 @@ export default function Input({
         name={name}
         value={value}
         onChange={handleChange}
-        className="py-4 px-6 bg-bg-card w-full rounded-sm"
+        className="py-2 px-4 bg-bg-card w-full rounded-sm"
         placeholder={placeholder}
+        onWheel={(e) => e.target.blur()}
         required
       />
     </>
