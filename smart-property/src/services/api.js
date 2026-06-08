e0f70c6c63eeb40fcc71265d8c710f1246683a64
Dwi Reza <dwireza1002@gmail.com>
2026-06-08 10:19:59 +0800

    feat: membuat fungsi api ke backend

---
+++ smart-property/src/services/api.js
@@ -0,0 +1,22 @@
+const BASE_URL = "http://127.0.0.1:5000";
+
+export async function predictionHouse(data) {
+  const config = {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify(data),
+  };
+
+  const response = await fetch(`${BASE_URL}/prediction/predict`, config);
+
+  const result = await response.json();
+
+  if (!result.success) {
+    throw new Error(result.messages);
+  }
+
+  console.log(result);
+  return result.prediction;
+}
